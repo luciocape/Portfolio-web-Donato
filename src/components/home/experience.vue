a9670b92d09eac69133df518a511bd22314976c3
luciocape <luciodimascapezzuto@gmail.com>
2024-04-11 22:40:09 -0300

    Hice sección de galeria responsive, pero falta para tamaños de tablet y celular

--- src/components/home/experience.vue
+++ src/components/home/experience.vue
@@ -1,9 +1,6 @@
 <template>
-	<article
-		v-for="(item, index) in experiencies"
-		:key="index"
-		class="experience-container d-flex w-100 mb-3 p-4 rounded-4 align-items-center"
-	>
+	<article v-for="(item, index) in experiencies" :key="index"
+		class="experience-container d-flex w-100 mb-3 p-4 rounded-3 align-items-center">
 		<div class="title">
 			<h3 class="mb-0">{{ item.company }}</h3>
 			<p class="mb-0">{{ item.job }}</p>
@@ -12,12 +9,8 @@
 		<div class="ms-3 summary">
 			<p class="mb-1">{{ item.description }}</p>
 			<div class="d-flex flex-wrap gap-2">
-				<span
-					v-for="(element, index) in item.tags"
-					:key="index"
-					class="tag bg-primary bg-opacity-25 text-info rounded-5 text-nowrap"
-					>{{ element }}</span
-				>
+				<span v-for="(element, index) in item.tags" :key="index"
+					class="tag bg-primary bg-opacity-25 text-info rounded-5 text-nowrap">{{ element }}</span>
 			</div>
 		</div>
 	</article>
@@ -59,6 +52,7 @@ const experiencies = [
 	border: 1px solid transparent;
 	transition: 250ms;
 }
+
 .experience-container:hover {
 	background-color: #5e97f32d;
 	border-top: 1px solid rgba(245, 245, 245, 0.158);
@@ -66,15 +60,19 @@ const experiencies = [
 
 	// --bs-bg-opacity: .01;
 }
+
 .title {
 	width: 30%;
 }
+
 .summary {
 	width: 65%;
+
 	.tag {
 		padding: 5px 10px;
 	}
 }
+
 .duration,
 .tag {
 	font-size: 0.8rem;
